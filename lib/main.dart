bc81b80dc019ff44ecf3a0561e034b8c6c4195fb
yogitah-mecha <107405454+yogitah-mecha@users.noreply.github.com>
2026-06-24 16:17:24 +0530

    feat: support show fps (#5)
    * fix: set up ObjectBox library; add font family to pubspec;
    
    * fix: build error
    
    * chore: show fps

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:mechanix_contacts/core/theme/app_theme.dart';
 import 'package:mechanix_contacts/features/contacts/data/repositories/contacts_repository.dart';
 import 'package:mechanix_contacts/features/contacts/data/repositories/contacts_repository_impl.dart';
@@ -6,6 +8,7 @@ import 'package:mechanix_contacts/features/contacts/presentation/screens/contact
 import 'package:mechanix_contacts/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:show_fps/show_fps.dart';
 
 void main() {
   runApp(
@@ -35,8 +38,15 @@ class DialerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final showFps = Platform.environment['SHOW_FPS'] == 'true';
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      builder: showFps
+          ? (context, child) {
+              return ShowFPS(visible: showFps, showChart: false, child: child!);
+            }
+          : null,
       theme: AppTheme.darkTheme,
       home: const ContactsScreen(),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
